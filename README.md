51a7c6b9956026d1ca39f889b98f909d9880022e
sk-40 <github.limes827@passmail.com>
2025-08-13 04:04:09 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Overview
+- A simple script to downloading videos from Youtube and then converting them to mp3.
+- Created 3.24.2025
+
+## How to use
+- Activate the .venv environment
+- Use with the syntax: python3 download_yt_audio.py -f [file name]
+- If using a proxy such as piped or Invideous, add domain name to line 4 of script
+
+## Issues
+- Converting to mp3 results in a somewhat corrupted file, switching to m4a for now
---
+++ download_yt_audio.py
@@ -0,0 +1,46 @@
+import getopt, sys
+import yt_dlp
+
+replace_proxy_url = ['piped.kavin.rocks']
+
+
+def main(file_name):
+    url_list = []
+    save_path = 'downloads/'
+
+    file = open(file_name)
+    for line in file.readlines():
+        for rpu in replace_proxy_url:
+            if rpu in line:
+                line = line.replace(rpu, 'www.youtube.com')
+        url_list.append(line)
+    file.close()
+
+    ydl_opts = {
+        'outtmpl': save_path + '/%(title)s.%(ext)s',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'm4a',
+            'preferredquality': '0'
+        }]
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        for url in url_list:
+            error_code = ydl.download(url)
+
+
+if __name__ == '__main__':
+    file_name = ''
+
+    argv = sys.argv[1:]
+    opts, args = getopt.getopt(argv, 'f:', [])
+
+    for opt, arg in opts:
+        if opt in ['-f', '']:
+            file_name = arg
+
+    try:
+        main(file_name)
+    except FileNotFoundError:
+        print(f'Error: {file_name} file doesn\'t not found.')
